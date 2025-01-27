01922969f9a75d7bd49f2fe3fb11430b0d66d908
Minseok Kim <mk7545@nyu.edu>
2025-01-27 18:52:52 +0400

    edited main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-changed by someone on main branch 
-by both and merged properly
-=======
-<<<<<<< HEAD
-edited by both and merged properly
-and now going to use pull request to merge
-:=======
- by both and merged properly
->>>>>>> main
-:wq
->>>>>>> 56d418e6c8e1ee83331a5f51ec5669459687bc60
+edited
